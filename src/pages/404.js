c1c7ebb1ff8274638953470a4188459bfa49e349
Rachael Thomas <rachaelthomas@rachaels-mbp.lan>
2023-01-23 15:14:43 -0700

    Add console logs

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,16 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = () => {
+  console.log("this is a test")
+  return (
+    <Layout>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
-)
+  )
+}
+
 
 export const Head = () => <Seo title="404: Not Found" />
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = () => {
+  console.log("this is a test")
+  return(
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
+}
 
 export const Head = () => <Seo title="Page two" />
 
